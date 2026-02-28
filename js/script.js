fc1b2600b3794096c7e3a7416e4533de7ecc3f1b
kodamsrikanth <kodamsrikanth291@gmail.com>
2026-02-28 13:55:23 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// // Set the date for the offer expiration (1 day from now)
+// let countDownDate = new Date().getTime() + 24 * 60 * 60 * 1000;
+
+// // Update the countdown every second
+// let countdown = setInterval(function() {
+
+//   const now = new Date().getTime();
+//   let distance = countDownDate - now;
+
+//   // Reset automatically after 24 hours
+//   if (distance <= 0) {
+//     countDownDate = new Date().getTime() + 24 * 60 * 60 * 1000;
+//     distance = countDownDate - now;
+//   }
+
+//   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   document.getElementById("days1").innerHTML = days.toString().padStart(2, "0");
+//   document.getElementById("hours1").innerHTML = hours.toString().padStart(2, "0");
+//   document.getElementById("minutes1").innerHTML = minutes.toString().padStart(2, "0");
+//   document.getElementById("seconds1").innerHTML = seconds.toString().padStart(2, "0");
+
+// }, 1000);
+
+
+
+// 24 hour countdown
+let countdownDate = new Date().getTime() + (24 * 60 * 60 * 1000);
+
+setInterval(function(){
+
+    let now = new Date().getTime();
+    let distance = countdownDate - now;
+
+    let days = Math.floor(distance / (1000*60*60*24));
+    let hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    let minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+    let seconds = Math.floor((distance % (1000*60)) / 1000);
+
+    document.getElementById("days").innerHTML = String(days).padStart(2,'0');
+    document.getElementById("hours").innerHTML = String(hours).padStart(2,'0');
+    document.getElementById("minutes").innerHTML = String(minutes).padStart(2,'0');
+    document.getElementById("seconds").innerHTML = String(seconds).padStart(2,'0');
+
+},1000);
+
+
+
+
+
+
+
+
+
+
+
+document.querySelectorAll('.faq .accordion-container .accordion').forEach(accordion => {
+    accordion.onclick = () => {
+        accordion.classList.toggle('active');
+    }
+})
